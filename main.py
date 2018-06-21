63595e43cd85cb3fa7e28888ff40d81209acdc36
chinmay55 <chinmayprabhakar55@gmail.com>
2018-06-21 19:19:20 +0200

    first-commit

--- main.py
+++ main.py
@@ -15,7 +15,53 @@ import glob
 import random
 
 # colors for the bboxes
+Extension = '.ppm'
 COLORS = ['red', 'blue', 'yellow', 'pink', 'cyan', 'green', 'black']
+labels = {
+0 : 'speed limit 20 (prohibitory)',
+1 : 'speed limit 30 (prohibitory)',
+2 : 'speed limit 50 (prohibitory)',
+3 : 'speed limit 60 (prohibitory)',
+4 : 'speed limit 70 (prohibitory)',
+5 : 'speed limit 80 (prohibitory)',
+6 : 'restriction ends 80 (other)',
+7 : 'speed limit 100 (prohibitory)',
+8 : 'speed limit 120 (prohibitory)',
+9 : 'no overtaking (prohibitory)',
+10 : 'no overtaking (trucks) (prohibitory)',
+11 : 'priority at next intersection (danger)',
+12 : 'priority road (other)',
+13 : 'give way (other)',
+14 : 'stop (other)',
+15 : 'no traffic both ways (prohibitory)',
+16 : 'no trucks (prohibitory)',
+17 : 'no entry (other)',
+18 : 'danger (danger)',
+19 : 'bend left (danger)',
+20 : 'bend right (danger)',
+21 : 'bend (danger)',
+22 : 'uneven road (danger)',
+23 : 'slippery road (danger)',
+24 : 'road narrows (danger)',
+25 : 'construction (danger)',
+26 : 'traffic signal (danger)',
+27 : 'pedestrian crossing (danger)',
+28 : 'school crossing (danger)',
+29 : 'cycles crossing (danger)',
+30 : 'snow (danger)',
+31 : 'animals (danger)',
+32 : 'restriction ends (other)',
+33 : 'go right (mandatory)',
+34 : 'go left (mandatory)',
+35 : 'go straight (mandatory)',
+36 : 'go right or straight (mandatory)',
+37 : 'go left or straight (mandatory)',
+38 : 'keep right (mandatory)',
+39 : 'keep left (mandatory)',
+40 : 'roundabout (mandatory)',
+41 : 'restriction ends (overtaking) (other)',
+42 : 'restriction ends (overtaking (trucks)) (other)'
+}
 # image sizes for the examples
 SIZE = 256, 256
 
@@ -70,7 +116,7 @@ class LabelTool():
         self.parent.bind("s", self.cancelBBox)
         self.parent.bind("a", self.prevImage) # press 'a' to go backforward
         self.parent.bind("d", self.nextImage) # press 'd' to go forward
-        self.mainPanel.grid(row = 1, column = 1, rowspan = 4, sticky = W+N)
+        self.mainPanel.grid(row = 1, column = 1, rowspan = 8, sticky = W+N)
 
         # showing bbox info & delete bbox
         self.lb1 = Label(self.frame, text = 'Bounding boxes:')
@@ -80,15 +126,25 @@ class LabelTool():
         self.btnDel = Button(self.frame, text = 'Delete', command = self.delBBox)
         self.btnDel.grid(row = 3, column = 2, sticky = W+E+N)
         self.btnClear = Button(self.frame, text = 'ClearAll', command = self.clearBBox)
-        self.btnClear.grid(row = 4, column = 2, sticky = W+E+N)
+        self.btnClear.grid(row = 5, column = 2, sticky = W+E+N)
+        #Getting the labels on display
+        self.listboxOption = Listbox(self.frame,width = 22, height = 32)
+        [self.listboxOption.insert(x,y) for x,y in labels.items()]
+        self.listboxOption.grid(row = 6, column = 2, sticky = N)
 
         # control panel for image navigation
         self.ctrPanel = Frame(self.frame)
-        self.ctrPanel.grid(row = 5, column = 1, columnspan = 2, sticky = W+E)
+        self.ctrPanel.grid(row = 9, column = 1, columnspan = 2, sticky = W+E)
         self.prevBtn = Button(self.ctrPanel, text='<< Prev', width = 10, command = self.prevImage)
         self.prevBtn.pack(side = LEFT, padx = 5, pady = 3)
         self.nextBtn = Button(self.ctrPanel, text='Next >>', width = 10, command = self.nextImage)
         self.nextBtn.pack(side = LEFT, padx = 5, pady = 3)
+
+        #Skip Button Start
+        self.nextBtn = Button(self.ctrPanel, text='Skip >>', width=10, command=self.skip_image)
+        self.nextBtn.pack(side=LEFT, padx=5, pady=3)
+        #Skip Button ends
+
         self.progLabel = Label(self.ctrPanel, text = "Progress:     /    ")
         self.progLabel.pack(side = LEFT, padx = 5)
         self.tmpLabel = Label(self.ctrPanel, text = "Go to Image No.")
@@ -131,9 +187,11 @@ class LabelTool():
 ##            return
         # get image list
         self.imageDir = os.path.join(r'./Images', '%03d' %(self.category))
-        self.imageList = glob.glob(os.path.join(self.imageDir, '*.JPEG'))
+        #print self.imageDir
+        self.imageList = glob.glob(os.path.join(self.imageDir, '*'+Extension))
+        #print self.imageList 
         if len(self.imageList) == 0:
-            print 'No .JPEG images found in the specified dir!'
+            print 'No ',Extension,' images found in the specified dir!'
             return
 
         # default to the 1st image in the collection
@@ -148,15 +206,18 @@ class LabelTool():
         # load example bboxes
         self.egDir = os.path.join(r'./Examples', '%03d' %(self.category))
         if not os.path.exists(self.egDir):
+            print "exiting, no examples"
             return
-        filelist = glob.glob(os.path.join(self.egDir, '*.JPEG'))
+        filelist = glob.glob(os.path.join(self.egDir, '*'+Extension))
         self.tmp = []
         self.egList = []
         random.shuffle(filelist)
+        print("execution reached")
         for (i, f) in enumerate(filelist):
             if i == 3:
                 break
             im = Image.open(f)
+            print "opened ",im
             r = min(SIZE[0] / im.size[0], SIZE[1] / im.size[1])
             new_size = int(r * im.size[0]), int(r * im.size[1])
             self.tmp.append(im.resize(new_size, Image.ANTIALIAS))
@@ -179,6 +240,8 @@ class LabelTool():
         self.clearBBox()
         self.imagename = os.path.split(imagepath)[-1].split('.')[0]
         labelname = self.imagename + '.txt'
+        #print "imagename is ",self.imagename
+        #print "labelname is", labelname
         self.labelfilename = os.path.join(self.outDir, labelname)
         bbox_cnt = 0
         if os.path.exists(self.labelfilename):
@@ -200,9 +263,23 @@ class LabelTool():
 
     def saveImage(self):
         with open(self.labelfilename, 'w') as f:
-            f.write('%d\n' %len(self.bboxList))
+            #Our change where we try to write the bounding box value
+            #f.write('%d\n' %len(self.bboxList))
+            #f.write('%d\n', self.listboxOption.get(self.listboxOption.curselection()))
             for bbox in self.bboxList:
                 f.write(' '.join(map(str, bbox)) + '\n')
+        #Save the current image in the new flder as well
+        self.imagefilename = os.path.join(self.outDir, self.imagename)
+        print 'original file name is ', self.imagename
+        print 'new file name is ',self.imagefilename
+        with open(self.imagefilename + Extension, 'w') as f:
+            self.img.save(f)
+        #Delete file
+        try:
+            remove_name = os.path.join(self.imageDir, self.imagename + Extension)
+            os.remove(remove_name)
+        except:
+            print 'please skip'
         print 'Image No. %d saved' %(self.cur)
 
 
@@ -212,10 +289,11 @@ class LabelTool():
         else:
             x1, x2 = min(self.STATE['x'], event.x), max(self.STATE['x'], event.x)
             y1, y2 = min(self.STATE['y'], event.y), max(self.STATE['y'], event.y)
-            self.bboxList.append((x1, y1, x2, y2))
+            selection = self.listboxOption.get(self.listboxOption.curselection())
+            self.bboxList.append((self.get_index(selection), x1, y1, x2, y2))
             self.bboxIdList.append(self.bboxId)
             self.bboxId = None
-            self.listbox.insert(END, '(%d, %d) -> (%d, %d)' %(x1, y1, x2, y2))
+            self.listbox.insert(END, '%s : (%d, %d) -> (%d, %d)' %(selection, x1, y1, x2, y2 ))
             self.listbox.itemconfig(len(self.bboxIdList) - 1, fg = COLORS[(len(self.bboxIdList) - 1) % len(COLORS)])
         self.STATE['click'] = 1 - self.STATE['click']
 
@@ -272,12 +350,22 @@ class LabelTool():
             self.cur += 1
             self.loadImage()
 
+    def skip_image(self, event = None):
+        if self.cur < self.total:
+            self.cur += 1
+            self.loadImage()
+
     def gotoImage(self):
         idx = int(self.idxEntry.get())
         if 1 <= idx and idx <= self.total:
             self.saveImage()
             self.cur = idx
             self.loadImage()
+    #Custom method for getting index
+    def get_index(self, value):
+        for key,label in labels.items():
+            if value == label:
+                return key
 
 ##    def setImage(self, imagepath = r'test2.png'):
 ##        self.img = Image.open(imagepath)
